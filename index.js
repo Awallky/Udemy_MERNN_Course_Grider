910efca0687cabc8b583d88a06c5ea8dcbb3a86f
Adam <avvallky@gmail.com>
2021-07-29 09:55:46 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
-const passport = require('passport')
+const passport = require('passport');
+const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
+
+mongoose.connect(
+  keys.mongoURI, 
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
 const app = express();
 
-mongoose.connect(keys.mongoURI, { 
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-}, (error) => {
-    if (error) {
-        console.log('Failure to connect to DB.');
-    }
-    else {
-        console.log('Connected to DB.');
-    }
-});
-
-// cookie lasts 30 days
-// 30 days, 24 hours, 60 minutes, 60 seconds, 1000 milliseconds
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [ keys.cookieKey ]
-    })
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
-// set up routes for server
+
 require('./routes/authRoutes')(app);
 
-// passed in from Heroku, or use default value of 4000
-const DEFAULT_PORT = 4000;
-const PORT_NO = process.env.PORT || DEFAULT_PORT;
-app.listen(PORT_NO);
+const PORT = process.env.PORT || 4000;
+app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -5,14 +5,13 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-    User.findById(id)
-        .then(user => {
-            done(null, user);
-        })
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
 // Enable app authentication with Google Strategy,
@@ -29,7 +28,7 @@ passport.use(
       const existingUser = await User.findOne({ googleId: profile.id });
       if (existingUser) {
         // we already have a record with the given profile ID
-        done(null, existingUser);
+        return done(null, existingUser);
       } else {
         // we don't have a user record with this ID, make a new record!
         const user = await new User({ googleId: profile.id }).save()
