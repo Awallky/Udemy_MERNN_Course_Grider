9a4faedf9d132e114891369e9b187de5cc096669
Adam <avvallky@gmail.com>
2021-08-10 13:43:43 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ mongoose.connect(
 
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+ 
+// parse application/json
+app.use(express.json());
+// Set up middleware for cookies and OAuth APIs
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -26,6 +32,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT);
