31656b57a77c07cfa14eadb2d7da11078014251c
Adam <avvallky@gmail.com>
2021-08-23 20:35:13 -0700

    Updating versions. Removing dead code.

--- config/keys.js
+++ config/keys.js
@@ -6,7 +6,3 @@ if (process.env.NODE_ENV == 'production') {
     // we are in development - return the dev keys
     module.exports = require('./dev')
 }
-// prod
-//mongodb+srv://Cluster0_admin:ylVZSuwqN082ZTnY@cluster0.6opfm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-// client_ID 83721774550-fc25qdvlqkk9i5suesoprt6j592qinj6.apps.googleusercontent.com
-// client secret 1h3-1-YNnqya0h82Ndmo836b
