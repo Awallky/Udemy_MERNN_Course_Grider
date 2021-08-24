e567a41edfc8aeb1f8c636a63dc6e260a41b7f42
Adam <avvallky@gmail.com>
2021-08-23 18:39:25 -0700

    Refactor cookie session and middleware.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
 const keys = require('./config/keys');
+
 require('./models/User');
 require('./services/passport');
-
 mongoose.connect(
   keys.mongoURI, 
   { 
@@ -15,22 +13,12 @@ mongoose.connect(
 );
 
 const app = express();
-
 // parse application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
- 
+app.use(express.urlencoded({ extended: true })); 
 // parse application/json
 app.use(express.json());
-// Set up middleware for cookies and OAuth APIs
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
 
+require('./services/cookies')(app);
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
